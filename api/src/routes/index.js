6af8e70668eac48a03d04ec9c58ebee2ccb662be
AleSaturno <alesaturno64@gmail.com>
2023-06-04 23:06:11 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,33 @@
 const { Router } = require('express');
+require('dotenv').config();
+const {API_KEY} = process.env;
+const axios = require('axios');
+const {Videogame} = require('../db');
+const express = require('express');
+const {Op} = require('sequelize');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// Informacion de la API en GET
+
+const GetApiInfo = async () =>{
+    const paginaDeJuegos = [];
+    let apiURL = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+    while(paginaDeJuegos.length < 5) {
+        paginaDeJuegos.push(apiURL.data.results)
+        apiURL = await axios.get(apiURL.data.next);
+    }
+    const apiInfo = paginaDeJuegos.flat().map((event) => {
+        return ({
+            id: event.id,
+            name: event.name,
+            image: e.background_image,
+        })
+    })
+}
 
 
 module.exports = router;
