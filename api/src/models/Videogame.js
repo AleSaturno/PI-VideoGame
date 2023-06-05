b31a11f0ba970d7df84e5351af9fd814dfe22baf
AleSaturno <alesaturno64@gmail.com>
2023-06-04 23:06:09 -0300

    Update Videogame.js

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,50 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+
+    id:{
+      type: DataTypes.UUID,
+      defaulValue: DataTypes.UUIDV4,
+      allowNullValue: false,
+      primaryKey: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    rating:{
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+
+    plataforms:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+
+    image:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    
+  },{
+    timestamps: false
   });
 };
