6e827d21e70d8d29306b604fd64b419dc052fde9
AleSaturno <alesaturno64@gmail.com>
2023-06-06 15:38:24 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -30,11 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genre } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Videogame.belongsToMany(Videogame, { through: 'videogame_related', as: 'relatedGames' });
+Videogame.belongsToMany( Genre, { through :'VideoGenre',timestamps : false});
+Genre.belongsToMany( Videogame, { through : 'VideoGenre',timestamps : false});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
