9829e6985f28ef6683f43fa12591d2982ad313c9
AleSaturno <alesaturno64@gmail.com>
2023-06-07 20:44:57 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,17 +9,14 @@ import {Provider} from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      
-        <App />
-      
-      
+      <App />
     </BrowserRouter>
   </Provider>
-)
-
+);
 
 
 // If you want to start measuring performance in your app, pass a function
