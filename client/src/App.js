74cfd29f470d728086c9f26b542004a37447b45b
AleSaturno <alesaturno64@gmail.com>
2023-06-10 13:06:04 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
+
+
 import {Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Landing from './components/Landing/Landing'
+import Home from './components/Home/Home'
+// import Detail from './components/Detail/Detail';
 
 
 const App = () =>{
+
   return (
+    
     <div className="App">
-      <Navbar/>
+      
+         <Navbar/>
 
       <Routes>
         <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        {/* <Route path='/detail/:id' element={<Detail/>}/> */}
+        
       </Routes>
     </div>
   );
