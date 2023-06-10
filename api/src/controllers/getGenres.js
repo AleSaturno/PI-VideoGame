b624b514d23eaad642d58f4510ab8de38f6bbceb
AleSaturno <alesaturno64@gmail.com>
2023-06-10 13:05:45 -0300

    Update getGenres.js

--- api/src/controllers/getGenres.js
+++ api/src/controllers/getGenres.js
@@ -11,21 +11,21 @@ const getGenres = async (req , res) =>{
         const db = await Genre.findAll()
         // Si no llega a ver nada, pido los generos de la API y los guardo en db
 
-        if(db.length === 0){
-            const respuesta = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
-
-            const genresMap = respuesta.data.results.map(gen =>{
-                return{
-                    id: gen.id,
-                    name: gen.name,
-                }
-            });
-            await Genre.bulkCreate(genresMap);;
-            // Aqui se retornar el array con los generos y si ya estaban en db, entonces retornara el resultado de la busqueda solicitada.
-            res.status(200).json(genresMap)
-        }else{
+        if(db.length){
             return res.status(200).json(db);
         }
+
+        const respuesta = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
+        const genresMap = respuesta.data.results.map(gen =>{
+            return{
+                id: gen.id,
+                name: gen.name,
+            }
+        });
+        await Genre.bulkCreate(genresMap);;
+        // Aqui se retornar el array con los generos y si ya estaban en db, entonces retornara el resultado de la busqueda solicitada.
+        res.status(200).json(genresMap)
+
     } catch (error) {
         res.status(404).send(error.message)
     }
