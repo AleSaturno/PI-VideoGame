d8895054c6a7942834f2008a30fb6570665acc71
AleSaturno <alesaturno64@gmail.com>
2023-06-10 13:06:17 -0300

    Create Home.jsx

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,32 @@
+import {useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+import Cards from '../Cards/Cards';
+
+
+const Home = () =>{
+
+    const allGames = useSelector(state => state.allGames);
+    const gamesByName = useSelector(state => state.gamesByName);
+
+
+    const [games , setGames] = useState([]);
+
+    useEffect(() =>{
+        if(gamesByName.length){
+            setGames(gamesByName)
+        }else if(allGames.length){
+            setGames(allGames)
+        }
+    }, [allGames, gamesByName])
+
+
+
+    return(
+        <div>
+            <Cards games = {games}/>
+        </div>
+    )
+}
+
+
+export default Home;
