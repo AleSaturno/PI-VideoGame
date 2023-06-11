de86b0bccef4a2e18fed3a831a1ee4dcedb2cd5e
AleSaturno <alesaturno64@gmail.com>
2023-06-11 16:48:41 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import {Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home'
-// import Detail from './components/Detail/Detail';
+import Detail from './components/Detail/Detail';
+import CreateForms from './components/CreateForm/CreateForms';
 
 
 const App = () =>{
@@ -19,7 +20,8 @@ const App = () =>{
       <Routes>
         <Route path='/' element={<Landing/>}/>
         <Route path='/home' element={<Home/>}/>
-        {/* <Route path='/detail/:id' element={<Detail/>}/> */}
+        <Route path='/detail/:id' element={<Detail/>}/>
+        <Route path='createForm' element={<CreateForms/>}/>
         
       </Routes>
     </div>
