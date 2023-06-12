1af6d901f6a676eddfdc8e84a938ce1d37fcb028
AleSaturno <alesaturno64@gmail.com>
2023-06-12 20:17:52 -0300

    Update Detail.jsx

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,38 +1,43 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import parser from 'html-react-parser';
 
 const Detail = () => {
   const { id } = useParams();
   const [gameDetail, setGameDetail] = useState({});
 
   useEffect(() => {
-        const getAxios = async () => {
-        const detail = await axios.get(`http://localhost:3001/videogames/${id}`);
-        setGameDetail(detail.data);
-        };
-        getAxios();
-    },[id]);
-  
- 
-  
+    const getAxios = async () => {
+      const detail = await axios.get(`http://localhost:3001/videogames/${id}`);
+      setGameDetail(detail.data);
+    };
+    getAxios();
+  }, [id]);
+
+  if (gameDetail.description) {
     return (
+      <div>
         <div>
-          {Object.keys(gameDetail).length > 0 ? (
-            <>
-              <h1>{gameDetail.name}</h1>
-              <h2>Genres: {gameDetail.genres?.map(genre => genre.name).join(' , ')}</h2>
-              <h2>Rating: {gameDetail.rating}</h2>
-              <h2>Platforms: {gameDetail.platforms?.platform && gameDetail.platforms.platform.map(platform => platform.name).join(', ')}</h2>
-              <h2>Released: {gameDetail.platforms?.released_at}</h2>
-              <h2>Requirements: {gameDetail.platforms?.requirements}</h2>
-            </>
-          ) : (
-            <p>Cargando...</p>
+          <h1>{gameDetail.name}</h1>
+          <img src={gameDetail.background_image} width="600px" height="auto" alt="Loading..." />
+          <h2>Genres: {gameDetail.genres.map(genre => genre.name).join(', ')}</h2>
+          <h2>Rating: {gameDetail.rating}</h2>
+          {gameDetail.platforms && gameDetail.platforms.length > 0 && (
+            <h2>Platforms: {gameDetail.platforms.map(platform => platform.platform.name).join(', ')}</h2>
           )}
+          <h2>Released: {gameDetail.released}</h2>
+          <h3>Description: {parser(gameDetail.description)}</h3>
         </div>
-      );
-      
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Cargando...</p>
+      </div>
+    );
+  }
 };
 
 export default Detail;
