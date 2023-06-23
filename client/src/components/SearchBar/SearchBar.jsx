c4c4328fed7abeffb08bd1d5c4cc6b83bd26c81b
AleSaturno <alesaturno64@gmail.com>
2023-06-23 12:18:57 -0300

    Update SearchBar.jsx

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
-import style from './SearchBar.module.css'
-const SearchBar = ({onSearch}) =>{
-    
-    const [name , setName] = useState('');
+import style from './SearchBar.module.css';
 
-    const handleChange = (event) =>{
-        setName(event.target.value);
+const SearchBar = ({ onSearch }) => {
+  const [name, setName] = useState('');
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    if (!/\d/.test(value)){
+        setName(value);
+        onSearch(value);
     }
+  };
 
-
-    return (
-        <div className={style.container}>
-            <input type="search" value={name} onChange={handleChange} />
-            <button onClick={() => onSearch(name)}>Busqueda</button>
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      <input type="search" value={name} onChange={handleChange} placeholder="Ingrese un juego" />
+      <button onClick={() => onSearch(name)}>Busqueda</button>
+    </div>
+  );
 }
-
 
 export default SearchBar;
