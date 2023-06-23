b14e8293a1d66e4063ab3ab0720dc928f80e2503
AleSaturno <alesaturno64@gmail.com>
2023-06-23 12:18:25 -0300

    Update postVideogame.js

--- api/src/controllers/postVideogame.js
+++ api/src/controllers/postVideogame.js
@@ -1,17 +1,16 @@
-const {Videogame} = require('../db');
+const { Videogame } = require('../db');
 
-const postVideogame = async (req , res) =>{
-    try {
-        const postGame = await Videogame.create(req.body)
-        await postGame.setGenres(req.body.genres)
-        const dbGames = await Videogame.findAll({
-            where : {name : req.body.name}
-        })
-        res.status(200).json(dbGames)
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-}
+const postVideogame = async (req, res) => {
+  try {
+    const postGame = await Videogame.create(req.body);
+    await postGame.setGenres(req.body.genres);
+    const dbGames = await Videogame.findAll({
+      where: { name: req.body.name },
+    });
+    res.status(200).json(dbGames);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = postVideogame;
-
