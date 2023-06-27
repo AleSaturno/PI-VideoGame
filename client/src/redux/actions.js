68c44e2350f75d26a95a51d776ad06fe20a87e4a
AleSaturno <alesaturno64@gmail.com>
2023-06-27 01:56:44 -0300

    Update actions.js

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const GET_ALLGAMES = 'GET_ALLGAMES';
 export const GET_GENRES = 'GET_GENRES';
@@ -45,7 +45,6 @@ export const byAll = () =>{
       type: GET_BYALL,
       payload: {}
     }
-         
-}
+  }    
 
 
